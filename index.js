a23a82ebfe1caccd0c90830b2919c33e65987e68
AminulIslamorange <aminulislamorange@gmail.com>
2024-12-18 22:02:51 +0600

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -62,6 +62,13 @@ async function run() {
       const result=await cartsCollection.insertOne(cartItem);
       res.send(result)
     });
+
+    app.delete('/carts/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id : new ObjectId(id)}
+      const result=await cartsCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
 
